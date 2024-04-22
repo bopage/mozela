7f5ec80e3096a03aa3c3c1eebcb6dc8b29829977
patrick <bpgeorges18@gmail.com>
2024-04-22 14:53:00 +0100

    Add Pre-Prod stage

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-import {  Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { CdkEBStage } from './eb-stage';
 
 /**
  * The stack that defines the application pipeline
@@ -13,21 +14,34 @@ export class CdkPipelineStack extends Stack {
       // The pipeline name
       pipelineName: 'MyServicePipeline',
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('bopage/mozela', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         installCommands: ['npm i -g npm@latest'],
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+      // How it will be built and synthesized
+      synth: new ShellStep('Synth', {
+        // Where the source can be found
+        input: CodePipelineSource.gitHub('bopage/mozela', 'main'),
+
+        // Install dependencies, build and run cdk synth
+        installCommands: ['npm i -g npm@latest'],
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ],
+      }),
     });
 
     // This is where we add the application stages
+    // This is where we add the application stages
+
+    // deploy beanstalk app
+    // For environment with all default values:
+    // const deploy = new CdkEBStage(this, 'Pre-Prod');
+
+    // For environment with custom AutoScaling group configuration
+    const deploy = new CdkEBStage(this, 'Pre-Prod', {
+      minSize: "1",
+      maxSize: "2"
+    });
+    const deployStage = pipeline.addStage(deploy);
+    
   }
 }
